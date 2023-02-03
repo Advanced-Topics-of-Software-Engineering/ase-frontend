35b43a2dcf95f9ea9e903c3e7722f05916e6b92c
İlgin Balkan <balkani17@itu.edu.tr>
2023-02-03 03:28:54 +0100

    Add alert and dialog messages for pickup boxes page

--- src/pages/pickup-boxes/PickupBoxes.js
+++ src/pages/pickup-boxes/PickupBoxes.js
@@ -3,39 +3,40 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../theme/Theme";
 import Header from "../Header";
 import { DataGrid } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
+import { Box, Button, Snackbar, Alert } from "@mui/material";
 import "../../App.css";
 import axios from "axios";
 import baseURL from "../../API";
 import CustomAccordion from "../Accordion";
+import ResponsiveDialog from "../ResponsiveDialog";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 250 },
+  { field: "id", headerName: "ID", flex: 1 },
   {
     field: "name",
     headerName: "Name",
-    width: 100,
+    flex: 1,
     editable: true,
-    sortable: false,
   },
   {
     field: "address",
     headerName: "Address",
-    width: 150,
+    flex: 1,
     editable: true,
-    sortable: false,
-  },
-  {
-    field: "alive",
-    headerName: "Alive",
-    width: 100,
-    editable: true,
-    sortable: false,
   },
 ];
 
 const PickupBoxes = () => {
   const [boxes, setBoxes] = useState([]);
+  const [updatedBoxes, setUpdatedBoxes] = useState([]);
+  const [isOpenDialog, setIsOpenDialog] = useState(false);
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [error, setError] = useState();
+
+  const handleProcessRowUpdate = (newRow, oldRow) => {
+    setUpdatedBoxes([...updatedBoxes, newRow]);
+  };
+
   const getBoxes = () => {
     axios
       .get(`${baseURL}/box`, {
@@ -46,16 +47,34 @@ const PickupBoxes = () => {
           id: "",
           name: "",
           streetAddress: "",
-          alive: "",
         },
       })
       .then((response) => {
         setBoxes(response.data);
-        console.log(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
+  };
+
+  const updateBoxes = () => {
+    for (let i = 0; i < updatedBoxes.length; i++) {
+      const newBox = {
+        name: updatedBoxes[i].name,
+        streetAddress: updatedBoxes[i].address,
+      };
+      axios
+        .post(`${baseURL}/box/update/${updatedBoxes[i].id}`, newBox)
+        .then((response) => {
+          console.log(response.status);
+          setError("");
+          setIsOpenAlert(true);
+        })
+        .catch((error) => {
+          setError(error.toJSON().message);
+          setIsOpenAlert(true);
+        });
+    }
   };
 
   useEffect(() => {
@@ -84,14 +103,13 @@ const PickupBoxes = () => {
               id: box.id,
               name: box.name,
               address: box.streetAddress,
-              alive: box.alive,
             }))}
             columns={columns}
             editMode="row"
             pageSize={7}
-            rowsPerPageOptions={[5]}
             disableSelectionOnClick
             experimentalFeatures={{ newEditingApi: true }}
+            processRowUpdate={handleProcessRowUpdate}
             style={{
               marginTop: 10,
               backgroundColor: "#fcfbfa",
@@ -100,6 +118,43 @@ const PickupBoxes = () => {
               boxShadow: "5",
             }}
           />
+          <Button
+            type="submit"
+            sx={{ borderRadius: 1 }}
+            variant="contained"
+            color="primary"
+            style={{
+              height: "40px",
+              width: "200px",
+              marginTop: "10px",
+            }}
+            onClick={() => {
+              setIsOpenDialog(true);
+            }}
+          >
+            Submit Changes
+          </Button>
+          <ResponsiveDialog
+            isOpen={isOpenDialog}
+            handleClose={() => setIsOpenDialog(false)}
+            title="Are you sure?"
+            handleYesClick={updateBoxes}
+          />
+          {isOpenAlert && (
+            <Snackbar
+              open={isOpenAlert}
+              autoHideDuration={6000}
+              onClose={() => setIsOpenAlert(null)}
+            >
+              <Alert
+                onClose={() => setIsOpenAlert(null)}
+                severity={error === "" ? "success" : "error"}
+                sx={{ width: "100%" }}
+              >
+                {error === "" ? "Values successfully changed" : error}
+              </Alert>
+            </Snackbar>
+          )}
         </Box>
       </div>
     </ThemeProvider>
