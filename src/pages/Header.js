9dcc405cbce2571488aee3371e10c7896db090a5
İlgin Balkan <balkani17@itu.edu.tr>
2023-02-04 19:31:31 +0100

    Update Header tabs based on logged in users' role

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Container,
@@ -7,26 +7,42 @@ import {
   Box,
   AppBar,
 } from "@mui/material";
-import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = [
+const dispatcherTabs = [
+  {
+    name: "Dispatchers",
+    endpoint: "dispatchers",
+  },
+  { name: "Customers", endpoint: "customers" },
   {
     name: "Deliverers",
     endpoint: "deliverers",
   },
-  { name: "Customers", endpoint: "customers" },
   { name: "Pickup Boxes", endpoint: "pickup-boxes" },
   {
     name: "Deliveries",
     endpoint: "deliveries",
   },
+];
+
+const userTabs = [
   {
-    name: "Add new user",
-    endpoint: "sign-up",
+    name: "Deliveries",
+    endpoint: "user-deliveries",
   },
 ];
 
 function Header() {
+  const [userType, setUserType] = useState("");
+
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    console.log("header", user);
+    if (user !== null) {
+      setUserType(user.userType);
+    }
+  });
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -48,20 +64,46 @@ function Header() {
           >
             ASE - 23
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}></Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                href={page.endpoint}
-                sx={{ my: 2, mx: 3, color: "white", display: "block" }}
-              >
-                {page.name}
-              </Button>
-            ))}
+            {userType === "ROLE_DISPATCHER"
+              ? dispatcherTabs.map((tab, index) => (
+                  <Button
+                    key={index}
+                    href={tab.endpoint}
+                    sx={{ my: 2, mx: 4, color: "white", display: "block" }}
+                  >
+                    {tab.name}
+                  </Button>
+                ))
+              : userType === ("ROLE_DELIVERER" || "ROLE_CUSTOMER") &&
+                userTabs.map((tab, index) => (
+                  <Button
+                    key={index}
+                    href={tab.endpoint}
+                    sx={{ my: 2, mx: 4, color: "white", display: "block" }}
+                  >
+                    {tab.name}
+                  </Button>
+                ))}
           </Box>
+          {userType !== "" ? (
+            <Button
+              href={"/"}
+              onClick={() => {
+                sessionStorage.clear();
+              }}
+              sx={{ color: "white", justifyContent: "flex-end" }}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              href={"/home"}
+              sx={{ color: "white", justifyContent: "flex-end" }}
+            >
+              Login
+            </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
