189d3ce3564026930932233328e244de47c28aae
İlgin Balkan <balkani17@itu.edu.tr>
2023-02-04 21:42:24 +0100

    Create Input reusable component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { TextField, IconButton, InputAdornment } from "@mui/material";
+import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import "./Input.css";
+
+function Input({ name, type = "text", label, onChange, isSecured = false }) {
+  const [value, setValue] = useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    onChange(event);
+  };
+
+  return !isSecured ? (
+    <TextField
+      fullWidth
+      required
+      onChange={handleChange}
+      name={name}
+      value={value}
+      margin="normal"
+      type={type}
+      variant="outlined"
+      label={label}
+      InputProps={{
+        classes: {
+          notchedOutline: "input-border",
+        },
+      }}
+      InputLabelProps={{
+        classes: {
+          root: "inputLabel",
+          focused: "inputLabel",
+        },
+      }}
+    />
+  ) : (
+    <TextField
+      fullWidth
+      required
+      onChange={handleChange}
+      name={name}
+      value={value}
+      margin="normal"
+      type={showPassword ? "text" : "password"}
+      variant="outlined"
+      label={label}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              edge="end"
+            >
+              {showPassword ? (
+                <VisibilityOffRoundedIcon />
+              ) : (
+                <VisibilityRoundedIcon />
+              )}
+            </IconButton>
+          </InputAdornment>
+        ),
+        classes: {
+          notchedOutline: "input-border",
+        },
+      }}
+      InputLabelProps={{
+        classes: {
+          root: "inputLabel",
+          focused: "inputLabel",
+        },
+      }}
+      sx={{
+        ".MuiSvgIcon-root ": {
+          fill: "#660090",
+        },
+      }}
+    />
+  );
+}
+
+export default Input;
